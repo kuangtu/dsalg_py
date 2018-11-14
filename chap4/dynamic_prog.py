53ebc09f6f19fb2db87b06bbb70156f6c7750139
kuangtu <kuangtuweb@gmail.com>
2018-11-14 16:17:48 +0800

    dynamic

---
+++ chap4/dynamic_prog.py
@@ -0,0 +1,64 @@
+
+def recMC(coinValueList,change):
+   minCoins = change
+   if change in coinValueList:
+     return 1
+   else:
+       #这个地方需要注意，硬币列中的值需要小于零钱
+      for i in [c for c in coinValueList if c <= change]:
+         numCoins = 1 + recMC(coinValueList,change-i)
+         if numCoins < minCoins:
+            minCoins = numCoins
+   return minCoins
+
+def recDC1(coinValueList,change,knownResults):
+   minCoins = change
+   if change in coinValueList:
+      knownResults[change] = 1
+      return 1
+   elif knownResults[change] > 0:
+      return knownResults[change]
+   else:
+       for i in [c for c in coinValueList if c <= change]:
+         numCoins = 1 + recDC1(coinValueList, change-i,
+                              knownResults)
+         if numCoins < minCoins:
+            minCoins = numCoins
+            #对于change所需要的最小数目硬币进行保存
+            knownResults[change] = minCoins
+   return minCoins
+
+
+def dpMakeChange(coinValueList,change,minCoins,coinsUsed):
+   for cents in range(change+1):
+      coinCount = cents
+      newCoin = 1
+      for j in [c for c in coinValueList if c <= cents]:
+            if minCoins[cents-j] + 1 < coinCount:
+               coinCount = minCoins[cents-j]+1
+               newCoin = j
+      minCoins[cents] = coinCount
+      coinsUsed[cents] = newCoin
+   return minCoins[change]
+
+def printCoins(coinsUsed,change):
+   coin = change
+   while coin > 0:
+      thisCoin = coinsUsed[coin]
+      print(thisCoin)
+      coin = coin - thisCoin
+
+if __name__ == '__main__':
+    # print(recMC([1,5,10,25],63))
+    # print(recDC1([1,5,10,25],63,[0]*64))
+    amnt = 63
+    clist = [1,5,10,21,25]
+    coinsUsed = [0]*(amnt+1)
+    coinCount = [0]*(amnt+1)
+
+    print("Making change for",amnt,"requires")
+    print(dpMakeChange(clist,amnt,coinCount,coinsUsed),"coins")
+    print("They are:")
+    printCoins(coinsUsed,amnt)
+    print("The used list is as follows:")
+    print(coinsUsed)
